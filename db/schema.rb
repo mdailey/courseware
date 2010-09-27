4aa2ad2821f3332d7aec2894cdbbb0be1cc38a5b
Matthew N. Dailey <mdailey@ait.ac.th>
2010-09-28 04:44:20 +0700

    Move session storage to database

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100924231512) do
+ActiveRecord::Schema.define(:version => 20100927210524) do
 
   create_table "announcements", :force => true do |t|
     t.integer  "course_id"
@@ -244,6 +244,16 @@ ActiveRecord::Schema.define(:version => 20100924231512) do
 
   add_index "roles_users", ["role_id"], :name => "index_roles_users_on_role_id"
   add_index "roles_users", ["user_id"], :name => "index_roles_users_on_user_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
