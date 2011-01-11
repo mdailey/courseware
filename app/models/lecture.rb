38909820078543b3b4f36a4b5f4a425b6bf7da5f
Matthew N. Dailey <mdailey@ait.ac.th>
2011-01-12 04:48:00 +0700

    Auto default lecture number

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -3,6 +3,14 @@ class Lecture < ActiveRecord::Base
   has_many :lecture_dates, :autosave => true
   validates_presence_of :course
 
+  def after_initialize
+    if new_record?
+      if self.number.nil?
+        self.number = ActiveRecord::Base.connection.execute("select max(number) from lectures where course_id=#{self.course_id}").first["max"].to_i + 1
+      end
+    end
+  end
+
   def lecture_dates_string
     lecture_dates.sort {|d1,d2| d1.date <=> d2.date}.collect { |d| d.date.to_formatted_s(:rfc822) }.join('<br/>')
   end
